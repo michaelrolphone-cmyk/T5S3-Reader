0884a6cd247706d0c86fa457e504b3b018862911
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-14 15:05:50 -0600

    Export native button remap API

--- lib/NativeApps/src/NativeAppLauncher.c
+++ lib/NativeApps/src/NativeAppLauncher.c
@@ -11,6 +11,7 @@
 #include "esp_elf.h"
 #include "T5AppApi.h"
 #include "T5BatteryApi.h"
+#include "T5ButtonRemapApi.h"
 #include "T5CacheApi.h"
 #include "T5FileBrowserApi.h"
 #include "T5FontApi.h"
@@ -65,6 +66,7 @@ esp_err_t launch_elf_app(const char *sd_path)
     static const struct esp_elfsym host_symbols[] = {
         ESP_ELFSYM_EXPORT(t5_app_get_api),
         ESP_ELFSYM_EXPORT(t5_battery_get_api),
+        ESP_ELFSYM_EXPORT(t5_button_remap_get_api),
         ESP_ELFSYM_EXPORT(t5_cache_get_api),
         ESP_ELFSYM_EXPORT(t5_font_get_api),
         ESP_ELFSYM_EXPORT(t5_koreader_get_api),
