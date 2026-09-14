efd407cbe23b075c55c245d7f8cae8c99aeb4d87
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-13 22:59:07 -0600

    activity: define manager lifecycle with complete Activity

--- src/activities/ActivityManagerDestructor.cpp
+++ src/activities/ActivityManagerDestructor.cpp
@@ -2,4 +2,10 @@
 
 #include <cassert>
 
+ActivityManager::ActivityManager(GfxRenderer& renderer, MappedInputManager& mappedInput)
+    : renderer(renderer), mappedInput(mappedInput), renderingMutex(xSemaphoreCreateMutex()) {
+  assert(renderingMutex != nullptr && "Failed to create rendering mutex");
+  stackActivities.reserve(10);
+}
+
 ActivityManager::~ActivityManager() { assert(false); }
