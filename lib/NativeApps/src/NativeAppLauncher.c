89f2f9a49038ae72029d877987908856a503527c
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-13 21:03:47 -0600

    native: register generic storage and system APIs

--- lib/NativeApps/src/NativeAppLauncher.c
+++ lib/NativeApps/src/NativeAppLauncher.c
@@ -9,8 +9,9 @@
 
 #include "esp_elf.h"
 #include "T5AppApi.h"
+#include "T5StorageApi.h"
+#include "T5SystemApi.h"
 #include "T5SystemUiApi.h"
-#include "T5TimecardApi.h"
 #include <errno.h>
 
 #if !CONFIG_IDF_TARGET_ESP32S3 || !CONFIG_ELF_LOADER_LOAD_PSRAM
@@ -47,8 +48,9 @@ esp_err_t launch_elf_app(const char *sd_path)
     }
     static const struct esp_elfsym host_symbols[] = {
         ESP_ELFSYM_EXPORT(t5_app_get_api),
+        ESP_ELFSYM_EXPORT(t5_storage_get_api),
+        ESP_ELFSYM_EXPORT(t5_system_get_api),
         ESP_ELFSYM_EXPORT(t5_system_ui_get_api),
-        ESP_ELFSYM_EXPORT(t5_timecard_get_api),
         ESP_ELFSYM_END
     };
     const int registered = esp_elf_register_symbol(host_symbols);
