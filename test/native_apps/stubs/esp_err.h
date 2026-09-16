b47881c40c8f83855da0db1189812a60e8efe4f6
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-16 14:49:13 -0600

    Add unsupported-capability error to isolated launcher test ESP-IDF stub

--- test/native_apps/stubs/esp_err.h
+++ test/native_apps/stubs/esp_err.h
@@ -5,5 +5,6 @@ typedef int esp_err_t;
 #define ESP_ERR_INVALID_ARG 0x102
 #define ESP_ERR_INVALID_STATE 0x103
 #define ESP_ERR_NOT_FOUND 0x105
+#define ESP_ERR_NOT_SUPPORTED 0x106
 #define ESP_ERR_NO_MEM 0x101
 static inline const char *esp_err_to_name(esp_err_t error) { (void)error; return "test error"; }
