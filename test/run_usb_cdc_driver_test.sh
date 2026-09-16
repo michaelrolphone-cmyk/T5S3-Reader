5fd4e07a38b3ac7a8338739471065662e27a99a4
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-16 05:54:29 -0600

    Exercise USB CDC loader with valid and mismatched ELF drivers

--- test/run_usb_cdc_driver_test.sh
+++ test/run_usb_cdc_driver_test.sh
@@ -5,6 +5,13 @@ build="$(mktemp -d)"
 trap 'rm -rf "$build"' EXIT
 cc -std=c11 -Wall -Wextra -Werror -fPIC -fvisibility=hidden -shared \
   -I"$repo/sdk/driver" "$repo/Drivers/usb_cdc/driver.c" -o "$build/usb-cdc.so"
+cc -std=c11 -Wall -Wextra -Werror -fPIC -fvisibility=hidden -shared \
+  -I"$repo/sdk/driver" -I"$repo/lib/NativeApps/include" \
+  "$repo/Drivers/gps_nmea/driver.c" -o "$build/gps.so"
 cc -std=c11 -Wall -Wextra -Werror -I"$repo/sdk/driver" \
   "$repo/test/drivers/usb_cdc_test.c" -ldl -o "$build/test"
 "$build/test" "$build/usb-cdc.so"
+c++ -std=c++17 -Wall -Wextra -Werror -I"$repo/sdk/driver" -I"$repo/src" \
+  -I"$repo/test/drivers/stubs" "$repo/src/runtime/drivers/UsbCdcDriverModule.cpp" \
+  "$repo/test/drivers/usb_cdc_module_test.cpp" -ldl -o "$build/module-test"
+"$build/module-test" "$build/usb-cdc.so" "$build/gps.so"
