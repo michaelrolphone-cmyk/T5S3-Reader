97acc84f687ec7b0186b0d671be0cbb267783f56
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-16 12:22:43 -0600

    Run device registry and owner lease host tests in driver CI

--- test/run_driver_test.sh
+++ test/run_driver_test.sh
@@ -13,6 +13,9 @@ c++ -std=c++17 -Wall -Wextra -Werror -I"$repo/sdk/driver" -I"$repo/lib/NativeApp
   "$repo/test/drivers/module_test.cpp" -ldl -o "$build/test"
 "$build/test" "$build/gps.so" "$build/bad.so"
 c++ -std=c++17 -Wall -Wextra -Werror -I"$repo/src" \
+  "$repo/test/resources/device_registry_test.cpp" -o "$build/device-registry-test"
+"$build/device-registry-test"
+c++ -std=c++17 -Wall -Wextra -Werror -I"$repo/src" \
   "$repo/test/programmer/esp_rom_protocol_test.cpp" -o "$build/esp-rom-protocol-test"
 "$build/esp-rom-protocol-test"
 c++ -std=c++17 -Wall -Wextra -Werror -I"$repo/src" -I"$repo/lib/NativeApps/include" \
