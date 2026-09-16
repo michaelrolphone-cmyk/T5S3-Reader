f2cd626a69b2df74fe096f74e530a7fb6d21b0a5
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-15 23:16:34 -0600

    Export serial.port capability to native apps

--- lib/NativeApps/src/NativeAppLauncher.c
+++ lib/NativeApps/src/NativeAppLauncher.c
@@ -25,6 +25,7 @@
 #include "T5OpdsApi.h"
 #include "T5OtaApi.h"
 #include "T5SdFirmwareApi.h"
+#include "T5SerialPortApi.h"
 #include "T5StatusBarApi.h"
 #include "T5StorageApi.h"
 #include "T5StreamApi.h"
@@ -78,6 +79,7 @@ esp_err_t launch_elf_app(const char *sd_path)
         ESP_ELFSYM_EXPORT(t5_opds_get_api),
         ESP_ELFSYM_EXPORT(t5_ota_get_api),
         ESP_ELFSYM_EXPORT(t5_sd_firmware_get_api),
+        ESP_ELFSYM_EXPORT(t5_serial_port_get_api),
         ESP_ELFSYM_EXPORT(t5_status_bar_get_api),
         ESP_ELFSYM_EXPORT(t5_storage_get_api),
         ESP_ELFSYM_EXPORT(t5_stream_get_api),
