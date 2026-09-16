14675f3edbf8ba9b848848026bf29bff338d6cce
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-16 06:53:51 -0600

    Run portable ESP ROM protocol regression in host CI

--- test/run_driver_test.sh
+++ test/run_driver_test.sh
@@ -12,6 +12,9 @@ c++ -std=c++17 -Wall -Wextra -Werror -I"$repo/sdk/driver" -I"$repo/lib/NativeApp
   -I"$repo/src" -I"$repo/test/drivers/stubs" "$repo/src/runtime/drivers/GpsDriverModule.cpp" \
   "$repo/test/drivers/module_test.cpp" -ldl -o "$build/test"
 "$build/test" "$build/gps.so" "$build/bad.so"
+c++ -std=c++17 -Wall -Wextra -Werror -I"$repo/src" \
+  "$repo/test/programmer/esp_rom_protocol_test.cpp" -o "$build/esp-rom-protocol-test"
+"$build/esp-rom-protocol-test"
 c++ -std=c++17 -Wall -Wextra -Werror -DBOARD_T5S3_PRO \
   -I"$repo/test/drivers/stubs" -I"$repo/src" "$repo/src/runtime/resources/RadioPower.cpp" \
   "$repo/test/drivers/power_test.cpp" -o "$build/power-test"
