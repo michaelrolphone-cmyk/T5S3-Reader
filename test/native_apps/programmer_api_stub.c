c9ae8ae24d0e84d3ffeb72fd2c06c9371fa3651f
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-16 15:30:30 -0600

    Stub transactional dependency binding in the isolated ELF launcher test

--- test/native_apps/programmer_api_stub.c
+++ test/native_apps/programmer_api_stub.c
@@ -7,14 +7,20 @@ const t5_program_esp_rom_api_v1 *t5_program_esp_rom_get_api(uint32_t version) {
     (void)version;
     return 0;
 }
-
 const t5_device_api_v1 *t5_device_get_api(uint32_t version) {
     (void)version;
     return 0;
 }
 
 int test_capability_gate_allowed = 1;
+int test_capability_bind_allowed = 1;
+int test_capability_bind_calls = 0;
 bool native_app_capabilities_ready(const char *sd_path) {
     (void)sd_path;
     return test_capability_gate_allowed != 0;
+}
+bool native_app_capabilities_bind(const char *sd_path) {
+    (void)sd_path;
+    ++test_capability_bind_calls;
+    return test_capability_bind_allowed != 0;
 }
