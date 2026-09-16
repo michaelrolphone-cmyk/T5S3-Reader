783239e0135d296df32b8b9b1ea405aeb4d8f5aa
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-16 13:48:10 -0600

    Run lease-bound location data plane under sanitizers in CI

--- test/run_stream_test.sh
+++ test/run_stream_test.sh
@@ -29,6 +29,10 @@ c++ -std=c++17 -Wall -Wextra -Werror -fsanitize=address,undefined -fno-omit-fram
 "$build/gnss-producer"
 c++ -std=c++17 -Wall -Wextra -Werror -fsanitize=address,undefined -fno-omit-frame-pointer \
   -I"$repo/lib/NativeApps/include" -I"$repo/src" \
+  "$repo/src/runtime/streams/StreamRuntime.cpp" "$repo/test/streams/location_lease_binding_test.cpp" -o "$build/location-lease-binding"
+"$build/location-lease-binding"
+c++ -std=c++17 -Wall -Wextra -Werror -fsanitize=address,undefined -fno-omit-frame-pointer \
+  -I"$repo/lib/NativeApps/include" -I"$repo/src" \
   "$repo/src/runtime/streams/StreamRuntime.cpp" "$repo/test/streams/http_transfer_test.cpp" -o "$build/http-transfer"
 "$build/http-transfer"
 printf '#include "T5StreamApi.h"\n#include "T5SerialPortApi.h"\n#include "RiscRteLocationRecords.h"\nint main(void) { return T5_STREAM_API_VERSION != 1 || T5_SERIAL_PORT_API_VERSION != 1 || RISCRTE_LOCATION_FIX_SIZE != 52; }\n' > "$build/abi.c"
