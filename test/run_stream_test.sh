cc0ab0d3c9edba1908a8c9c0e04a7e4a02047be1
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-16 13:17:45 -0600

    Run location subscription and device-revocation regression with stream host suite

--- test/run_stream_test.sh
+++ test/run_stream_test.sh
@@ -21,6 +21,10 @@ c++ -std=c++17 -Wall -Wextra -Werror -fsanitize=address,undefined -fno-omit-fram
 "$build/gnss-record"
 c++ -std=c++17 -Wall -Wextra -Werror -fsanitize=address,undefined -fno-omit-frame-pointer \
   -I"$repo/lib/NativeApps/include" -I"$repo/src" \
+  "$repo/src/runtime/streams/StreamRuntime.cpp" "$repo/test/streams/location_position_subscriptions_test.cpp" -o "$build/location-subscriptions"
+"$build/location-subscriptions"
+c++ -std=c++17 -Wall -Wextra -Werror -fsanitize=address,undefined -fno-omit-frame-pointer \
+  -I"$repo/lib/NativeApps/include" -I"$repo/src" \
   "$repo/src/runtime/streams/StreamRuntime.cpp" "$repo/test/streams/http_transfer_test.cpp" -o "$build/http-transfer"
 "$build/http-transfer"
 printf '#include "T5StreamApi.h"\n#include "T5SerialPortApi.h"\n#include "RiscRteLocationRecords.h"\nint main(void) { return T5_STREAM_API_VERSION != 1 || T5_SERIAL_PORT_API_VERSION != 1 || RISCRTE_LOCATION_FIX_SIZE != 52; }\n' > "$build/abi.c"
