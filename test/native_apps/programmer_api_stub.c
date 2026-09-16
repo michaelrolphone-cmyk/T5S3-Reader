91c17c8b443ea98e511dfdf4ac745583c8eed379
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-16 15:34:27 -0600

    Record and clear dependency reservation before loader teardown in host fixture

--- test/native_apps/programmer_api_stub.c
+++ test/native_apps/programmer_api_stub.c
@@ -15,6 +15,8 @@ const t5_device_api_v1 *t5_device_get_api(uint32_t version) {
 int test_capability_gate_allowed = 1;
 int test_capability_bind_allowed = 1;
 int test_capability_bind_calls = 0;
+int test_capability_active = 0;
+int test_capability_release_calls = 0;
 bool native_app_capabilities_ready(const char *sd_path) {
     (void)sd_path;
     return test_capability_gate_allowed != 0;
@@ -22,5 +24,13 @@ bool native_app_capabilities_ready(const char *sd_path) {
 bool native_app_capabilities_bind(const char *sd_path) {
     (void)sd_path;
     ++test_capability_bind_calls;
-    return test_capability_bind_allowed != 0;
+    if (!test_capability_bind_allowed) return false;
+    test_capability_active = 1;
+    return true;
+}
+void native_app_capabilities_release(void) {
+    if (test_capability_active) {
+        test_capability_active = 0;
+        ++test_capability_release_calls;
+    }
 }
