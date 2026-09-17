c703393d591ac5e90c58cb309e5e46500141d1ce
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-17 13:51:01 -0600

    Declare stub ELF initialization/relocation for actual privileged manifest-gate unit test

--- test/drivers/stub_privileged_loader/esp_elf.h
+++ test/drivers/stub_privileged_loader/esp_elf.h
@@ -11,6 +11,8 @@ typedef struct esp_elf {
     uint16_t num;
     esp_symtab_t *symtab;
 } esp_elf_t;
+int esp_elf_init(esp_elf_t *module);
+int esp_elf_relocate(esp_elf_t *module, const uint8_t *image);
 void esp_elf_deinit(esp_elf_t *module);
 #ifdef __cplusplus
 }
