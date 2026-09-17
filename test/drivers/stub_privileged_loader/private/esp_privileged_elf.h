30033e95ae134fd71194e35794388f6831a8145a
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-17 13:49:55 -0600

    Align host snapshot relocation stub with mandatory signed import arguments

--- test/drivers/stub_privileged_loader/private/esp_privileged_elf.h
+++ test/drivers/stub_privileged_loader/private/esp_privileged_elf.h
@@ -7,7 +7,9 @@ extern "C" {
 #endif
 int esp_elf_relocate_privileged_verified_v1(esp_elf_t *module,
                                             const uint8_t *image,
-                                            size_t length);
+                                            size_t length,
+                                            const char *const *signed_imports,
+                                            size_t signed_import_count);
 #ifdef __cplusplus
 }
 #endif
