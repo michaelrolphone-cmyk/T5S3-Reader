8ce8168e3c13dfb59a0b12e98f330acc346c25b2
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-16 18:42:37 -0600

    Export offline driver manager API to native applications

--- lib/NativeApps/src/NativeAppLauncher.c
+++ lib/NativeApps/src/NativeAppLauncher.c
@@ -15,6 +15,7 @@
 #include "T5CacheApi.h"
 #include "T5DeviceApi.h"
 #include "T5DriverManagerApi.h"
+#include "T5DriverOfflineApi.h"
 #include "T5FileBrowserApi.h"
 #include "T5FontApi.h"
 #include "T5GpsApi.h"
@@ -86,6 +87,7 @@ esp_err_t launch_elf_app(const char *sd_path)
         ESP_ELFSYM_EXPORT(t5_cache_get_api),
         ESP_ELFSYM_EXPORT(t5_device_get_api),
         ESP_ELFSYM_EXPORT(t5_driver_manager_get_api),
+        ESP_ELFSYM_EXPORT(t5_driver_offline_get_api),
         ESP_ELFSYM_EXPORT(t5_font_get_api),
         ESP_ELFSYM_EXPORT(t5_koreader_get_api),
         ESP_ELFSYM_EXPORT(t5_language_get_api),
