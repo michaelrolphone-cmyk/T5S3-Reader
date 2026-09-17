ab8002b023dabb3995bcc112a6ebfd271a813321
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-17 12:51:07 -0600

    Run compiled native import-policy negative tests alongside scope tests

--- test/run_privileged_os_cpu_v1_test.sh
+++ test/run_privileged_os_cpu_v1_test.sh
@@ -9,3 +9,9 @@ trap 'rm -f "$exe"' EXIT
   lib/elf_loader/src/esp_privileged_os_cpu.c \
   test/drivers/privileged_os_cpu_v1_test.c -o "$exe"
 "$exe"
+"${CC:-cc}" -std=gnu11 -O2 -fno-builtin -fno-stack-protector \
+  -Wall -Wextra -Werror \
+  -Itest/drivers/stub_os_cpu -Ilib/elf_loader/include \
+  lib/elf_loader/src/esp_privileged_imports.c \
+  test/drivers/privileged_imports_v1_test.c -o "$exe"
+"$exe"
