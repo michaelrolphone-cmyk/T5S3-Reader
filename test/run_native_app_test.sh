2475a2f39d8bfe2c3f9d6d44335f434f8ba38f2c
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-18 14:40:20 -0600

    Invoke serial launch regression with bash to avoid non-executable file mode

--- test/run_native_app_test.sh
+++ test/run_native_app_test.sh
@@ -30,5 +30,5 @@ fi
 python3 "$repo_dir/test/native_apps/test_symbols.py"
 python3 "$repo_dir/test/native_apps/test_capability_manifest.py"
 python3 "$repo_dir/test/resources/driver_install_stack_progress_source_test.py"
-"$repo_dir/test/run_serial_launch_contract.sh"
+bash "$repo_dir/test/run_serial_launch_contract.sh"
 echo 'Native app launcher tests passed'
