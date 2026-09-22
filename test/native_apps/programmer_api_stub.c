d910481f55f609dce21e7601de574f90337c29d0
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-21 18:00:14 -0600

    Teach isolated launcher test stub the new generic provider API and cleanup hook

--- test/native_apps/programmer_api_stub.c
+++ test/native_apps/programmer_api_stub.c
@@ -2,6 +2,7 @@
 #include "T5DeviceApi.h"
 #include "T5LocationApi.h"
 #include "T5PackageManagerApi.h"
+#include "T5ProviderCapabilityApi.h"
 #include <stdbool.h>
 
 /* The launcher test links an isolated host symbol table, not the firmware. */
@@ -20,6 +21,13 @@ const t5_location_api_v1 *t5_location_get_api(uint32_t version) {
 const t5_package_manager_api_v1 *t5_package_manager_get_api(uint32_t version) {
     (void)version;
     return 0;
+}
+const t5_provider_capability_api_v1 *t5_provider_capability_get_api(uint32_t version) {
+    (void)version;
+    return 0;
+}
+void native_app_provider_capabilities_release(void) {
+    /* No provider is loaded in this test binary. */
 }
 
 int test_capability_gate_allowed = 1;
