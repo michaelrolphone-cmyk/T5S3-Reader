c9f03462ebff2b97743d724e85775a3db4c0bbe1
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-22 16:51:59 -0600

    Run Home shortcut handoff regression in native app tests

--- test/run_native_app_test.sh
+++ test/run_native_app_test.sh
@@ -30,6 +30,7 @@ else
 fi
 python3 "$repo_dir/test/native_apps/test_symbols.py"
 python3 "$repo_dir/test/native_apps/test_capability_manifest.py"
+python3 "$repo_dir/test/native_apps/home_shortcut_launch_contract_test.py"
 python3 "$repo_dir/test/resources/driver_install_stack_progress_source_test.py"
 bash "$repo_dir/test/run_serial_launch_contract.sh"
 cc -std=c11 -Wall -Wextra -Werror \
