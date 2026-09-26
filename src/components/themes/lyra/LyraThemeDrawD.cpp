b1c93526e8699986df1f6dda497d2f07f3f40c14
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-26 08:42:27 -0600

    Restore authored Font Awesome face for Home icons

--- src/components/themes/lyra/LyraThemeDrawD.cpp
+++ src/components/themes/lyra/LyraThemeDrawD.cpp
@@ -53,7 +53,7 @@ void LyraTheme::drawButtonMenu(GfxRenderer& renderer, Rect rect, int buttonCount
       constexpr int kAppIconSize = 12;
       const int iconX = textX + (mainMenuIconSize - kAppIconSize) / 2;
       const int iconY = tileRect.y + (LyraMetrics::values.menuRowHeight - kAppIconSize) / 2;
-      (void)FontAwesomeIcons::drawRegular(renderer, iconX, iconY, appIcon, kAppIconSize, true);
+      (void)FontAwesomeIcons::draw(renderer, iconX, iconY, appIcon, kAppIconSize, true);
       textX += mainMenuIconSize + hPaddingInSelection + 2;
     } else if (rowIcon != nullptr) {
       UIIcon icon = rowIcon(i);
