0b2482a593162209882cd0d7e926bd982fd1a251
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-26 07:46:11 -0600

    Remove touch hack regression runner

--- test/run_idle_power_test.sh
+++ test/run_idle_power_test.sh
@@ -13,5 +13,4 @@ for board in BOARD_T5S3_PRO BOARD_LILYGO_EPD47_S3; do
 done
 cc -std=c11 -Wall -Wextra -Werror -I"$repo/lib/bq25896/include" \
   "$repo/test/hal/usb_input_power_test.c" -o "$build/input-test"
-python3 "$repo/test/hal/touch_latency_source_test.py"
 "$build/input-test"
