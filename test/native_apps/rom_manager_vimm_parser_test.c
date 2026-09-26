c37bd65c7270dca0327a3d5ed124ca0bed02a38d
michaelrolphone-cmyk <michael.rol.phone@gmail.com>
2026-09-26 10:31:51 -0600

    Exercise Rom Manager persistent Vimm diagnostics

--- test/native_apps/rom_manager_vimm_parser_test.c
+++ test/native_apps/rom_manager_vimm_parser_test.c
@@ -22,6 +22,21 @@ static const t5_app_api_v1 kApp={
   .millis=fake_millis,
 };
 
+static bool fake_remove_file(const char *path){assert(path);return true;}
+static const t5_storage_api_v1 kStorage={
+  .api_version=T5_STORAGE_API_VERSION,
+  .struct_size=sizeof(t5_storage_api_v1),
+  .remove_file=fake_remove_file,
+};
+static bool fake_poll_event(t5_ui_event_t *event,uint32_t wait_ms){
+  (void)wait_ms;assert(event);memset(event,0,sizeof(*event));return true;
+}
+static const t5_ui_api_v1 kUi={
+  .api_version=T5_UI_API_VERSION,
+  .struct_size=sizeof(t5_ui_api_v1),
+  .poll_event=fake_poll_event,
+};
+
 static int32_t fake_open_http(const char *url,t5_stream_t *out){
   assert(url&&out);
   assert(strcmp(url,"https://vimm.net/vault/?p=list&system=GB&section=W")==0);
@@ -37,13 +52,23 @@ static int32_t fake_read(t5_stream_t h,void *buffer,uint32_t cap,uint32_t *count
   served=true;
   return T5_STREAM_EOF;
 }
-static int32_t fake_close(t5_stream_t h){assert(h==1);return T5_STREAM_OK;}
+static int32_t fake_open_file(const char *path,uint32_t mode,t5_stream_t *out){
+  assert(path&&out);assert(mode==T5_STREAM_FILE_CREATE_NEW);*out=2;return T5_STREAM_OK;
+}
+static int32_t fake_write(t5_stream_t h,const void *buffer,uint32_t size,uint32_t *count){
+  assert(h==2&&buffer&&count);*count=size;return T5_STREAM_OK;
+}
+static int32_t fake_finish(t5_stream_t h){assert(h==2);return T5_STREAM_OK;}
+static int32_t fake_close(t5_stream_t h){assert(h==1||h==2);return T5_STREAM_OK;}
 
 static const t5_stream_api_v1 kStreams={
   .api_version=T5_STREAM_API_VERSION,
   .struct_size=sizeof(t5_stream_api_v1),
+  .open_file=fake_open_file,
   .open_http=fake_open_http,
   .read=fake_read,
+  .write=fake_write,
+  .finish=fake_finish,
   .close=fake_close,
 };
 
@@ -55,11 +80,15 @@ const t5_system_ui_api_v1 *t5_system_ui_get_api(uint32_t version){(void)version;
 const t5_ui_api_v1 *t5_ui_get_api(uint32_t version){(void)version;return 0;}
 
 int main(void){
-  char buffer[4096];
+  char buffer[4096],details[4096],debug[DEBUG_CAP];
   html=buffer;
-  detail_text=buffer;
+  detail_text=details;
+  debug_text=debug;
+  debug_text[0]=0;debug_size=0;
   app=&kApp;
+  storage=&kStorage;
   streams=&kStreams;
+  ui=&kUi;
   search_query[0]=0;
   status_text[0]=0;
 
@@ -71,5 +100,7 @@ int main(void){
   assert(strcmp(vimm_names[1],"Wario Land")==0);
   assert(strcmp(vimm_paths[1],"/vault/12345")==0);
   assert(vimm_kinds[1]==VIMM_GAME);
+  assert(strstr(debug_text,"parser_summary")!=0);
+  assert(strstr(debug_text,"numeric_first=2")!=0);
   return 0;
 }
